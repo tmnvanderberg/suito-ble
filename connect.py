56d78daf03fef8da8a02080a46ecb4a2959d401b
Timon van der Berg <tmnvanderberg@gmail.com>
2023-06-08 21:32:53 +0200

    worky

--- connect.py
+++ connect.py
@@ -1,7 +1,43 @@
 import asyncio
 import json
+from re import L
 
 from bleak import BleakClient, BleakScanner
+
+featureList = [
+"Average Speed Supported",
+"Cadence Supported",
+"Total Distance Supported",
+"Inclination Supported",
+"Elevation Gain Supported",
+"Pace Supported",
+"Step Count Supported",
+"Resistance Level Supported",
+"Stride Count Supported",
+"Expended Energy Supported",
+"Heart Rate Measurement Supported",
+"Metabolic Equivalent Supported",
+"Elapsed Time Supported",
+"Remaining Time Supported",
+"Power Measurement Supported",
+"Force on Belt and Power Output Supported",
+"User Data Retention Supported"]
+
+bikeDataList = [
+"More Data",
+"Average Speed",
+"Instantaneous Cadence",
+"Average Cadence",
+"Total Distance",
+"Resistance Level",
+"Instantaneous Power",
+"Average Power",
+"Expended Energy",
+"Heart Rate",
+"Metabolic Equivalent",
+"Elapsed Time",
+"Remaining Time"
+]
 
 def find_gatt_uuid(gatt, description):
     for primary in gatt:
@@ -10,16 +46,44 @@ def find_gatt_uuid(gatt, description):
         for characteristic in primary['characteristics']:
             if characteristic['description'] == description:
                 return characteristic['uuid']
-    return ""
+    raise NameError("gatt undefined")
 
 with open('output.json') as file:
     gatt = json.load(file)
 
-async def enable_notifications(client, characteristic_uuid):
-    await client.start_notify(characteristic_uuid, notification_callback)
+async def enable_notifications(client, characteristic_uuid, callback):
+    await client.start_notify(characteristic_uuid, callback)
 
-async def notification_callback(sender: int, data: bytearray):
+async def dummySubCallback(sender: int, data: bytearray):
     print("notification received", sender, data);
+
+async def bikeDataCallback(sender: int, data: bytearray):
+    print("sender:", sender);
+    for feature, byte in zip(bikeDataList, list(data)):
+        print(feature, ":", byte)
+
+def prettyPrintFeatures(featureList, byteList):
+    for feature, byte in zip(featureList, byteList):
+        print(feature, ":", byte)
+
+def dummyCallback(characteristic, characteristicValue):
+    print(characteristic, characteristicValue)
+
+def fitnessFeaturesCallback(characteristic, characteristicValue):
+    print(" = ", characteristic, " = ")
+    prettyPrintFeatures(featureList, characteristicValue) 
+    
+
+subscriptions = {'Indoor Bike Data' : bikeDataCallback,
+                 'Fitness Machine Status' : dummySubCallback,
+                 'Training Status': dummySubCallback
+                 }
+
+reads = {
+        'Supported Power Range' : dummyCallback,
+        'Supported Resistance Level Range': dummyCallback,
+        'Fitness Machine Feature': fitnessFeaturesCallback
+        }
 
 async def connect(deviceName):
     scanner = BleakScanner()
@@ -29,8 +93,12 @@ async def connect(deviceName):
         client = BleakClient(device)
         await client.connect()
         print('connected')
-        await enable_notifications(client, find_gatt_uuid(gatt, "Indoor Bike Data"))
-        print("entering sleep loop")
+        for characteristic, callback in reads.items():
+            characteristic_value = await client.read_gatt_char(find_gatt_uuid(gatt, characteristic))
+            callback(characteristic, list(characteristic_value))
+        for characteristic, callback in subscriptions.items():
+            await enable_notifications(client, find_gatt_uuid(gatt, characteristic), callback)
+        print("entering loop")
         while True:
             await asyncio.sleep(1) 
 
